e280485288159a625ad1ccfaeafd422efcdd75f8
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-28 00:15:55 -0400

    added on click function to add searched cities to local storage

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 // API key: 3b49a656fe3af7c1b318cdb7bf3447ef
 
-$("#Search").on("click", function () {
-    var city = $("#city").val()
+var cityName = document.querySelector("#cityName");
+var showTemperature = document.querySelector("#temperature");
+var showHumidity = document.querySelector("#humidity");
+var showWindSpeed = document.querySelector("#windSpeed");
+var showHistory = document.querySelector("#history");
+var cityInput = document.querySelector("#cityInput");
+var searchButton = document.querySelector("#searchButton");
+
+var citySearchHistory = [];
+var city = "Charlotte";
+
+renderCitySearchHistory();
+getCurrentWeatherSearch();
+
+// Adds search history to local storage
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    var cityValue = cityInput.value.trim();
+    if (cityValue !== "") {
+        city = cityValue;
+
+        citySearchHistory.push(city);
+        localStorage.setItem("Cities", JSON.stringify(citySearchHistory))
+        cityInput.value = "";
+        renderCitySearchHistory();
+        getCurrentWeatherSearch();
+    } else { return }
+});
+
+
+
+
+
+
+
+
+
+// Gets city from search box and retrieves from API
+$("#searchButton").on("click", function () {
+    var city = $("#cityInput").val()
     $.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=3b49a656fe3af7c1b318cdb7bf3447ef&units=imperial", function (data) {
         console.log(data)
         $("#temperature").text(data.main.temp)
@@ -10,6 +48,8 @@ $("#Search").on("click", function () {
         $("#history").prepend("<button>" + city + "</button>")
     })
 })
+
+
 
 $("#history").on("click", "button", function () {
     var city = $(this).text()
@@ -20,6 +60,15 @@ $("#history").on("click", "button", function () {
 
     })
 })
+
+// Stores and displays previous searches
+function 
+
+
+
+
+// Loads last searched city forecast on page load
+
 
 
 
